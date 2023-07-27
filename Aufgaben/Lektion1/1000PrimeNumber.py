86b662bbccc5b194b321f41e06c99589e0d99f76
RobynEismann <n.eismann1@gmx.de>
2023-07-27 10:33:58 +0200

    aufgabe 2

--- Aufgaben/Lektion1/1000PrimeNumber.py
+++ Aufgaben/Lektion1/1000PrimeNumber.py
@@ -7,6 +7,7 @@ while primeCounter != 1000:
     for i in range (2,number):      # testet ob number eine Primzahl ist
         if (number % i) == 0:       
             primeNumber = False     # dann ist number keine Primzahl
+            break
     if primeNumber == True:         
         primeCounter += 1           # ist eine Primzahl, also imkrementieren wir den Counter  
 
