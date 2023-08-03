852cfeb3250476baf778fdece20a768d777a9747
RobynEismann <n.eismann1@gmx.de>
2023-08-03 20:35:03 +0200

    Implementierung

---
+++ Aufgaben/Lektion4/aufgabe2.py
@@ -0,0 +1,25 @@
+# Entwickeln Sie eine Routine in Python, welche dem Textstring S = “Eine alte Dame geht heute
+# einkaufen.” einen Zahlenwert zuordnet, durch die Definition a = b mod 26. Dabei ist b ∈ Z ein
+# Zahlenwert, welcher den Index der Buchstabenfolge a, b, c, d, ..., bzw. A, B, C, D, ..., beschreibt
+# und a ∈ Z26 der resultierende Zahlenwert (mod 26) für einen Buchstaben aus dem Textstring S.
+#---------------------------------------------------------------------------
+
+
+
+def getAlphabetNumber(letter):                          # Diese Funktion berechnet den numerischen Wert eines Buchstabens im Alphabet
+    
+
+    res = ord(str(letter).lower())-ord('a')             # # Berechne den Abstand zum Buchstaben 'a'
+    return 0 if res > 25 or res < 0 else res            # sicherstellen, dass der Wert im Bereich von 0 bis 25 ist (mod 26)
+
+
+S = "Eine alte Dame geht heute einkaufen."
+sum = 0
+
+for letter in S:
+    number = getAlphabetNumber(letter)                  # Erhalte den numerischen Wert des Buchstabens
+    sum += number                                       # addieren der Werte aus dem String
+
+print(sum)
+
+
